8b65fe60297135d50eefc1eed3993590be308d2d
ycp <820363773@qq.com>
2025-10-23 17:16:00 +0800

    [feat] update table name

--- gorm/query.go
+++ gorm/query.go
@@ -3,9 +3,9 @@ package gorm
 import "gorm.io/gen"
 
 type Query interface {
-	// SELECT * FROM @@table WHERE id = @id LIMIT 1
+	// SELECT * FROM `@@table` WHERE id = @id LIMIT 1
 	GetByID(id uint64) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{where}}
 	//   {{if val != ""}}
 	//     {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
@@ -21,7 +21,7 @@ type Query interface {
 	// {{end}}
 	// LIMIT 1
 	GetByCol(col, val string) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{if len(cols) == len(vals)}}
 	//  {{where}}
 	//     {{for key1, col := range cols}}
@@ -45,7 +45,7 @@ type Query interface {
 	// {{end}}
 	// LIMIT 1
 	GetByCols(cols, vals []string) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{where}}
 	//   {{if val != ""}}
 	//     {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
@@ -60,7 +60,7 @@ type Query interface {
 	//   {{end}}
 	// {{end}}
 	FindByCol(col, val string) []gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{if len(cols) == len(vals)}}
 	//  {{where}}
 	//     {{for key1, col := range cols}}
